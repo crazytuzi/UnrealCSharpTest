87dc035dde6d07508dd002df892d480c6c52aa14
crazytuzi <957824770@qq.com>
2023-10-13 01:38:52 +0800

    Move Binding Register Implementation

--- Source/UnrealCSharpTest/Private/UnitTest/Binding/TestBindingPropertyActor.cpp
+++ Source/UnrealCSharpTest/Private/UnitTest/Binding/TestBindingPropertyActor.cpp
@@ -2,60 +2,16 @@
 
 
 #include "UnitTest/Binding/TestBindingPropertyActor.h"
-#include "Binding/Class/TBindingClassBuilder.inl"
 #include "Binding/Class/TReflectionClassBuilder.inl"
-#include "Binding/Enum/TBindingEnumBuilder.inl"
 #include "Macro/NamespaceMacro.h"
 
 BINDING_REFLECTION_CLASS(ATestBindingPropertyActor);
 
 BINDING_ENUM(ERawTestEnum)
 
-struct FRegisterRawTestEnum
-{
-	FRegisterRawTestEnum()
-	{
-		TBindingEnumBuilder<ERawTestEnum>()
-			.Enumerator("RawTestEnumZero", ERawTestEnum::RawTestEnumZero)
-			.Enumerator("RawTestEnumOne", ERawTestEnum::RawTestEnumOne)
-			.Enumerator("RawTestEnumTwo", ERawTestEnum::RawTestEnumTwo)
-			.Register();
-	}
-};
-
-static FRegisterRawTestEnum RegisterRawTestEnum;
-
 BINDING_ENUM(ERawTestEnumClass)
 
-struct FRegisterRawTestEnumClass
-{
-	FRegisterRawTestEnumClass()
-	{
-		TBindingEnumBuilder<ERawTestEnumClass>()
-			.Enumerator("RawTestEnumClassZero", ERawTestEnumClass::RawTestEnumClassZero)
-			.Enumerator("RawTestEnumClassOne", ERawTestEnumClass::RawTestEnumClassOne)
-			.Enumerator("RawTestEnumClassTwo", ERawTestEnumClass::RawTestEnumClassTwo)
-			.Register();
-	}
-};
-
-static FRegisterRawTestEnumClass RegisterRawTestEnumClass;
-
 BINDING_CLASS(FRawTestStruct);
-
-struct FRegisterRawTestStruct
-{
-	FRegisterRawTestStruct()
-	{
-		TBindingClassBuilder<FRawTestStruct>(NAMESPACE_BINDING)
-			.EqualTo()
-			.NotEqualTo()
-			.Property("Value", BINDING_PROPERTY(&FRawTestStruct::Value))
-			.Register();
-	}
-};
-
-static FRegisterRawTestStruct RegisterRawTestStruct;
 
 struct FRegisterTestBindingPropertyActor
 {
